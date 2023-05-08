a7d6573d1ae4eeed438a289d29a18cb99ee3ba37
Xiangdong Dai <m18001856137@gmail.com>
2023-05-08 16:29:16 -0400

    newly trained weights

--- trainer.py
+++ trainer.py
@@ -9,11 +9,11 @@ def map_data(num):
     return num / 255
 
 if __name__ == "__main__":
-    '''trainer = Train()
+    trainer = Train()
     trainer.train("mnist_train.csv", 100)
-    trainer.save()'''
+    trainer.save()
 
-    net = Network("weights.txt")
+    '''net = Network("weights.txt")
     train_data = np.array(pd.read_csv("mnist_test.csv"))
     test_input = train_data[5:6, 1:].transpose()
     test_input = np.vectorize(map_data)(test_input)
@@ -30,4 +30,4 @@ if __name__ == "__main__":
 
     print(f"label: {test_label}")
     print(f"result: {result}")
-    print(f"accuracy: {accuracy}")
+    print(f"accuracy: {accuracy}")'''
